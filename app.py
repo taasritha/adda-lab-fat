21e85cebf406cd99600fbd4a38d2ee93ff9cbf7c
aasrithaaa_thumms <145799237+taasritha@users.noreply.github.com>
2025-04-10 19:03:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+print("hello")
